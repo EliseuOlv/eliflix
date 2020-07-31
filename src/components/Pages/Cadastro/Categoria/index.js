f744888e430143d5ae50c987c1e47d5680a3ee10
Eliseu Oliveira <eliseu.imasd@gmail.com>
2020-07-31 00:28:45 -0300

    commit rapidão

---
+++ src/components/Pages/Cadastro/Categoria/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PageDefault from '../../../PageDefault';
+import { Link } from 'react-router-dom';
+
+function cadastroCategoria(){
+    return(
+        <PageDefault> 
+            <h1>Cadastro de categoria</h1>
+
+            <Link to="/">
+                Ir para a home
+            </Link>
+        </PageDefault>
+    )
+  }
+
+export default cadastroCategoria;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Pages/Home';
+import cadastroVideo from './components/Pages/Cadastro/Video';
+import cadastroCategoria from './components/Pages/Cadastro/Categoria';
+
+function pagina404(){
+  return(
+    <div>
+      error 404
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={cadastroVideo} />
+      <Route path="/cadastro/categoria" component={cadastroCategoria} />
+      <Route component={pagina404}/>
+    </Switch>
+
+  </BrowserRouter>,
   document.getElementById('root')
 );
